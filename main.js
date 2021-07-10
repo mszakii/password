1b05a95742573a27600811c06d078ed57d5636b7
Mohammed Zaky <62839447+mohammed-zaky@users.noreply.github.com>
2021-07-10 19:18:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ up.onclick = function get() {
   
   let code = document.querySelector(".code");
   
-  let car = "@#$_&!?/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@@##$$__&&**??%%"
+  let car = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
   ;
   let count = 24;
   let serial = "";
