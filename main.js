c763e4110612809f52b0c80f7f1948c69316b9bb
Mohammed Zaky <62839447+mohammed-zaky@users.noreply.github.com>
2021-07-10 12:52:27 +0200

    Upload Files

---
+++ main.js
@@ -0,0 +1,32 @@
+let up = document.querySelector(".gen");
+let go = document.querySelector(".ser");
+
+go.onclick = function get() {
+  
+  if (go.style.bottom == "-100%") {
+    go.style.bottom = "0";
+  } else {
+    go.style.bottom = "-100%";
+  }
+  
+}
+
+up.onclick = function get() {
+  
+  if (go.style.bottom == "-100%") {
+    go.style.bottom = "0";
+  } else {
+    go.style.bottom = "-100%";
+  }
+  
+  let code = document.querySelector(".code");
+  
+  let car = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$_&*?%"
+  ;
+  let count = 24;
+  let serial = "";
+  for (let i = 0; i < count; i++) {
+    serial += car[Math.floor(Math.random() * car.length)];
+  }
+  code.innerHTML = serial;
+}
